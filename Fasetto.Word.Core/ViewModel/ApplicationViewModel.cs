a349a0035702b418cc56d850e038af7fe986001f
mongooseasd <mongooseasd@gmail.com>
2018-09-05 14:44:25 +0200

    Basic chat message entry box and buttons with enlarge animation and minor style fixes

--- Fasetto.Word.Core/ViewModel/ApplicationViewModel.cs
+++ Fasetto.Word.Core/ViewModel/ApplicationViewModel.cs
@@ -3,11 +3,11 @@
 namespace Fasetto.Word.Core {
   /// <summary>   /// The application state as a view model   /// </summary>   public class ApplicationViewModel : BaseViewModel
   {
-    /// <summary>         /// The current page of the application         /// </summary>     public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Login;
+    /// <summary>         /// The current page of the application         /// </summary>     public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.Chat;
 
     /// <summary>
     /// True if the side menu should be showed
-    /// </summary>     public bool SideMenuVisible { get; set; } = false;
+    /// </summary>     public bool SideMenuVisible { get; set; } = true;
 
     /// <summary>
     /// Navigates to the specified page
