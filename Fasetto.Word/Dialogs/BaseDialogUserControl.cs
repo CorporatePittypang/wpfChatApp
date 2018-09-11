96ae75945a8e39e46a43b94140638340fe55e7f9
mongooseasd <mongooseasd@gmail.com>
2018-09-11 15:37:58 +0200

    Minor fixes,  Dialog message implementation 20%

--- Fasetto.Word/Dialogs/BaseDialogUserControl.cs
+++ Fasetto.Word/Dialogs/BaseDialogUserControl.cs
@@ -1,114 +1,122 @@
 ﻿using Fasetto.Word.Core;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
-  /// <summary>
-  /// The base class for any content that is being used inside of a <see cref="DialogWindow"/>
-  /// </summary>
-  public class BaseDialogUserControl : UserControl
-  {
-    #region Private Members
+	/// <summary>
+	/// The base class for any content that is being used inside of a <see cref="DialogWindow"/>
+	/// </summary>
+	public class BaseDialogUserControl : UserControl
+	{
+		#region Private Members
 
-    /// <summary>
-    /// The dialog window we will be contained within
-    /// </summary>
-    private DialogWindow mDialogWindow;
+		/// <summary>
+		/// The dialog window we will be contained within
+		/// </summary>
+		private DialogWindow mDialogWindow;
 
-    #endregion
-
-
-    #region Public Commands
+		#endregion
 
 
-    #endregion
+		#region Public Commands
 
 
-    #region Public Properties
-
-    /// <summary>
-    /// Minimum width of this dialog
-    /// </summary>
-    public int WindowMinimumWidth { get; set; } = 250;
-
-    /// <summary>
-    /// Minimum height of this dialog
-    /// </summary>
-    public int WindowMinimumHeight { get; set; } = 100;
-
-    /// <summary>
-    /// The height of the title bar
-    /// </summary>
-    public int TitleHeight { get; set; } = 30;
-
-    /// <summary>
-    /// The title fot this dialog
-    /// </summary>
-    public string Title { get; set; }
-
-    #endregion
+		#endregion
 
 
-    #region Constructor
-    /// <summary>
-    /// Default constructor
-    /// </summary>
-    public BaseDialogUserControl()
-    {
-      // Create a new dialog window
-      mDialogWindow = new DialogWindow();
-      mDialogWindow.ViewModel = new DialogWindowViewModel(mDialogWindow);
+		#region Public Properties
 
-      
-    }
-    #endregion
+		/// <summary>
+		/// Minimum width of this dialog
+		/// </summary>
+		public int WindowMinimumWidth { get; set; } = 250;
 
-    #region Public Dialog Show Methods
+		/// <summary>
+		/// Minimum height of this dialog
+		/// </summary>
+		public int WindowMinimumHeight { get; set; } = 100;
 
-    /// <summary>
-    /// Displays a single message box to the user
-    /// </summary>
-    /// <param name="viewModel">The view model</param>
-    /// <typeparam name="T">The view model type for this control</typeparam>
-    /// <returns></returns>
-    public Task ShowDialog<T>(T viewModel)
-      where T: BaseViewModel
-    {
+		/// <summary>
+		/// The height of the title bar
+		/// </summary>
+		public int TitleHeight { get; set; } = 30;
 
-      // Create a task to await the dialog closing
-      var tcs = new TaskCompletionSource<bool>();
+		/// <summary>
+		/// The title fot this dialog
+		/// </summary>
+		public string Title { get; set; }
 
-      // Run on UI thread
-      Application.Current.Dispatcher.Invoke(() =>
-      {
-        try
-        {
-          // Match controls expected sizes to the dialog windows view model
-          mDialogWindow.ViewModel.WindowMinimumWidth = WindowMinimumWidth;
-          mDialogWindow.ViewModel.WindowMinimumHeight = WindowMinimumHeight;
-          mDialogWindow.ViewModel.TitleHeight = TitleHeight;
-          mDialogWindow.ViewModel.Title = Title;
-
-          // Setup this controls datacontext bindint to the view model
-          DataContext = viewModel;
-
-          // Show dialog
-          mDialogWindow.ShowDialog();
-        }
-        finally
-        {
-          // Let caller know, we finished
-          tcs.TrySetResult(true);
-        }
-      });
-
-      return tcs.Task;
-    }
-
-    #endregion
+		#endregion
 
 
-  }
+		#region Constructor
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public BaseDialogUserControl()
+		{
+			if (!DesignerProperties.GetIsInDesignMode(this))
+			{
+				// Create a new dialog window
+				mDialogWindow = new DialogWindow();
+				mDialogWindow.ViewModel = new DialogWindowViewModel(mDialogWindow);
+
+				// Create close command
+				//CloseCommand = new RelayCommand(() => mDialogWindow.Close());
+			}
+		}
+		#endregion
+
+		#region Public Dialog Show Methods
+
+		/// <summary>
+		/// Displays a single message box to the user
+		/// </summary>
+		/// <param name="viewModel">The view model</param>
+		/// <typeparam name="T">The view model type for this control</typeparam>
+		/// <returns></returns>
+		public Task ShowDialog<T>(T viewModel)
+			where T: BaseViewModel
+		{
+
+			// Create a task to await the dialog closing
+			var tcs = new TaskCompletionSource<bool>();
+
+			// Run on UI thread
+			Application.Current.Dispatcher.Invoke(() =>
+			{
+				try
+				{
+					// Match controls expected sizes to the dialog windows view model
+					mDialogWindow.ViewModel.WindowMinimumWidth = WindowMinimumWidth;
+					mDialogWindow.ViewModel.WindowMinimumHeight = WindowMinimumHeight;
+					mDialogWindow.ViewModel.TitleHeight = TitleHeight;
+					mDialogWindow.ViewModel.Title = Title;
+
+					// Set this control to the dialog window content
+					mDialogWindow.ViewModel.Content = this;
+
+					// Setup this controls datacontext bindint to the view model
+					DataContext = viewModel;
+
+					// Show dialog
+					mDialogWindow.ShowDialog();
+				}
+				finally
+				{
+					// Let caller know, we finished
+					tcs.TrySetResult(true);
+				}
+			});
+
+			return tcs.Task;
+		}
+
+		#endregion
+
+
+	}
 }
--- Fasetto.Word/Dialogs/DialogMessageBox.xaml.cs
+++ Fasetto.Word/Dialogs/DialogMessageBox.xaml.cs
@@ -2,14 +2,14 @@
 
 namespace Fasetto.Word
 {
-    /// <summary>
-    /// Interaction logic for DialogMessageBox.xaml
-    /// </summary>
-    public partial class DialogMessageBox : BaseDialogUserControl
-    {
-        public DialogMessageBox()
-        {
-            InitializeComponent();
-        }
-  }
+		/// <summary>
+		/// Interaction logic for DialogMessageBox.xaml
+		/// </summary>
+		public partial class DialogMessageBox : BaseDialogUserControl
+	{
+				public DialogMessageBox()
+				{
+						InitializeComponent();
+				}
+		}
 }
