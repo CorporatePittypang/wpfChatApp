e2798359f698f89e9d627f34d9852b89e92b4de7
mongooseasd <mongooseasd@gmail.com>
2020-05-12 23:41:30 +0200

    Labelwidth constant value removed from TextEntryWidthMatcherProperty

--- Fasetto.Word/AttachedProperties/TextEntryWidthMatcherProperty.cs
+++ Fasetto.Word/AttachedProperties/TextEntryWidthMatcherProperty.cs
@@ -4,28 +4,28 @@ using System.Windows.Controls;
 
 namespace Fasetto.Word
 {
-	/// <summary>
-	/// Match the label width of all text entry controls inside this panel
-	/// </summary>
-	public class TextEntryWidthMatcherProperty : BaseAttachedProperty<TextEntryWidthMatcherProperty, bool>
-	{
-		public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-		{
-			// Get the panel (grid typically)
-			var panel = (sender as Panel);
+    /// <summary>
+    /// Match the label width of all text entry controls inside this panel
+    /// </summary>
+    public class TextEntryWidthMatcherProperty : BaseAttachedProperty<TextEntryWidthMatcherProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the panel (grid typically)
+            var panel = (sender as Panel);
 
             SetWidths(panel);
 
             // Wait for panel to load
             RoutedEventHandler onLoaded = null;
             onLoaded = (s, ee) =>
-			{
+            {
                 panel.Loaded -= onLoaded;
 
                 SetWidths(panel);
 
-				foreach(var child in panel.Children)
-				{
+                foreach (var child in panel.Children)
+                {
                     if (!(child is TextEntryControl control))
                         continue;
 
@@ -34,10 +34,10 @@ namespace Fasetto.Word
                         SetWidths(panel);
                     };
                 }
-			};
+            };
 
             panel.Loaded += onLoaded;
-		}
+        }
 
         // Set everything to the same, the maxSize
         private void SetWidths(Panel panel)
@@ -49,12 +49,11 @@ namespace Fasetto.Word
                 if (!(child is TextEntryControl control))
                     continue;
 
-                control.LabelWidth = GridLength.Auto;
-
-                maxSize = Math.Max(maxSize, control.RenderSize.Width + control.Label.Margin.Left + control.Label.Margin.Right);
+                maxSize = Math.Max(maxSize, control.Label.RenderSize.Width + control.Label.Margin.Left + control.Label.Margin.Right);
             }
 
             var gridLength = (GridLength)new GridLengthConverter().ConvertFromString(maxSize.ToString());
+
             foreach (var child in panel.Children)
             {
                 if (!(child is TextEntryControl control))
